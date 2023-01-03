4e989a848e9825b07b4ba995b447ec55ebf5601c
Christian Motta <cm@castinellc.com>
2023-01-03 18:15:14 -0300

    Setup Tailwind

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,7 @@
-<h1>Welcome to SvelteKit</h1>
+<script>
+  let count = 0;
+</script>
+
+<h1 class="p-4">Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<button on:click={() => count++}>Click count: {count}</button>
